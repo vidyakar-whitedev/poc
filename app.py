f7c60c271efc1b5142b0f29b613bcb3372b02f0f
vidyakar-whitedev <vidyakar21@gmail.com>
2026-03-11 22:19:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, kind + Trivy!"
+def home():
+    return "Hello Secure Flask with Chainguard! working"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
